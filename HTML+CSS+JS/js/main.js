a36fbb4b593410c751bfe2051dc42f979a038b96
Daniel <danielbacelardeveloper@gmail.com>
2023-01-27 09:28:07 -0300

    Iniciando HTML 001.5

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,7 +1,21 @@
-var btnContact = document.querySelector('.db-btn-contect');
+//Declarando variáveis
+var btnContact = document.querySelector('.db-btn-contact');
 
+//Page preloader
+
+window.addEventListener('load', function (){
+    var pagePreloader = this.document.querySelector('.db-preloader');
+        pagePreloader.classList.add('db-fade-out');
+
+    setTimeout(function(){
+        pagePreloader.style.display = 'none'
+    }, 1000);
+});
+
+//Abrindo e fechando informacoes de contato
 btnContact.addEventListener('click', function () {
-    var boxContact = document.querySelector('.db-contact-info');
+    var boxContact = document.querySelector('.db-contatct-info');
     boxContact.classList.toggle('db-is-open');
     this.classList.toggle('db-change-icon');
 });
+
